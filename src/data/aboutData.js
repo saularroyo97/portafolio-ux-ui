9e1d48c9d4ac3264ae1dddd97697521c0239c0f9
Saul Arroyo <106997439+saularroyo97@users.noreply.github.com>
2025-04-16 09:36:58 -0600

    Delete src directory

--- src/data/aboutData.js
+++
@@ -1,9 +0,0 @@
-
-const aboutData = {
-    title: "Sobre mí",
-    image: "/aboutData.png",
-    text: "Diseñador UX/UI con 6+ años liderando experiencias digitales para plataformas B2B, B2C y e- commerce. Especialista en prototipado interactivo (Figma, Adobe XD) y pruebas de usabilidad con enfoque en métricas de retención y conversión.",
-  };
-  
-  export default aboutData;
-  
